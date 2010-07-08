4b63cc9e60bef9a682716d5ba364a14c6ac4ee50
Pontus Enmark <pontus.enmark@cgi.com>
2010-07-08 13:35:03 +0000

    Remove py.test
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@696 484df14f-75d2-4b3f-8ba0-490e05069a01

--- sourcesharingplugin/tests/test_sourcesharer.py
+++ sourcesharingplugin/tests/test_sourcesharer.py
@@ -15,53 +15,6 @@ from trac.web.tests.chrome import Request
 from trac.resource import Resource
 from trac.web.href import Href
 
-# py.test
-#smtp_port = 2525
-#
-#def start_server(smtp_port):
-#    server = SMTPThreadedServer(port=smtp_port)
-#    server.start()
-#    return server
-#
-#def pytest_funcarg__server(request):
-#    return request.cached_setup(
-#        setup=lambda: start_server(smtp_port),
-#        teardown=lambda server: server.stop(),
-#        scope='module'
-#    )
-#
-#def pytest_funcarg__sharesys(request):
-#    # Set up environment
-#    env = EnvironmentStub(enable=['trac.*', 'announcer.*', 'sourcesharingplugin.*'])
-#    if sourcesharingplugin.sourcesharer.using_announcer:
-#        env.config.set('smtp', 'port', smtp_port)
-#    else:
-#        env.config.set('notifications', 'smtp_port', smtp_port)
-#    ss = SharingSystem(env)
-#    return ss
-#
-#class TestSharingSystem:
-#    def test_send_mail(self, server, sharesys):
-#        dir = resource_filename(__name__, os.path.join('..', 'htdocs'))
-#        files = [os.path.join(dir, f) for f in os.listdir(dir)]
-#        subject = 'Sending söme files'
-#        body = 'Here you go (Här får du)'
-#        b64body = body.encode('base64').strip() 
-#        mail = sharesys.send_as_email(('Pöntus Enmärk', 
-#                                              'pontus.enmark@logica.com'), 
-#                                              [('Pontus Enmark', 
-#                                                'pontus.enmark@logica.com'),
-#                                               ('Pöntus Enmärk',
-#                                                'pontus.enmark@gmail.com')], 
-#                                                subject,
-#                                                body,
-#                                                *files)
-#        headers, sent_body = parse_smtp_message(server.store.message)
-#        assert  b64body in sent_body, (b64body, sent_body)
-#        assert 'söme'.decode('utf-8') in headers['Subject'], headers
-#        assert os.path.basename(files[0]) in sent_body
-
-# nose
 class SharingSystemTestCase(unittest.TestCase):
     @classmethod
     def setupClass(cls):
